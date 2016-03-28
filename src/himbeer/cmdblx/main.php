0f03dd1335d9fa087c9e3c4c5a3af1c6296e0a90
HimbeersaftLP <elias.nestl@outlook.de>
2016-03-28 18:49:31 +0200

    Create main.php

---
+++ src/himbeer/cmdblx/main.php
@@ -0,0 +1,29 @@
+<?php
+namespace himbeer\cmdblx;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\permission\Permission;
+use pocketmine\utils\Config;
+class Main extends PluginBase implements Listener{
+     
+     public function onEnable(){
+          $this->getServer()->getPluginManager()->registerEvents($this,$this);
+          $this->getLogger()->info("VIPcmds enabled!");
+          @mkdir($this->getDataFolder());
+          $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array(
+               #cinfig file for cmdblx
+          ));
+          $this->saveResource("config.yml");
+     }
+     
+     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+          switch($command->getName()){
+               case "cmdblx":
+                   if
+          }
+          return true;
+     }
+}
